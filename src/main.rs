7abf9fae79553c1ae9dbfdfa8d6459ede59a812c
Some Cho <itssomicho@gmail.com>
2024-01-29 20:07:33 +0100

    implement CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
+use clap::Parser;
 use metrum::{scanner, score::Score};
 
-fn main() {
-    // let file = std::fs::read_to_string("examples/valid/eine-kleine-nachtmusik").unwrap();
-    let file = String::from(
-        "
-        h=120
-        | q q q q |  1/3x3  | 1/5x5 | qx4|
-                            ",
-    );
+#[derive(Parser, Debug)]
+#[command(author,version,about,long_about=None)]
+struct Args {
+    #[arg(short, long)]
+    path: String,
+}
+
+fn main() -> Result<(), hound::Error> {
+    let args = Args::parse();
+    let file = std::fs::read_to_string(&args.path).unwrap();
+    let output_path = format!("{}.wav", &args.path.split('/').last().unwrap());
     let tokens = scanner::scan(file).unwrap();
     let score = Score::new(tokens).unwrap();
-    score.write_click_track("test.wav");
-    println!("{}", score);
+    score.write_click_track(&output_path)
 }
